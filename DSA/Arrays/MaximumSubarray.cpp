7b879936ee0f32822bf7736e501f5e270b01a1a6
upendraTRL <96102665+upendraTRL@users.noreply.github.com>
2023-04-26 23:35:51 +0530

    Create MaximumSubarray.cpp

---
+++ DSA/Arrays/MaximumSubarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int max = INT_MIN;
+
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+
+            if(max < sum){
+                max = sum;
+            }
+
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+
+        return max;
+    }
+};
