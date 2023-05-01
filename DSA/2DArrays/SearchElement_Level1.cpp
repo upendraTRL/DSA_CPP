6e33f5db08402e0fd7c5944efa3517a674369fd3
upendraTRL <96102665+upendraTRL@users.noreply.github.com>
2023-05-01 16:00:19 +0530

    Create SearchElement_Level1.cpp

---
+++ DSA/2DArrays/SearchElement_Level1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int start = 0;
+        int end = (row*col)-1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            int element = matrix[mid/col][mid%col];
+
+            if(target == element){
+                return 1;
+            }
+
+            if(target > element){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return 0;
+    }
+};
