003e8098c49546cd0bcbafe55b05adb213078fc9
upendraTRL <96102665+upendraTRL@users.noreply.github.com>
2023-07-21 16:52:27 +0530

    Create 2SumVariety1Optimal.cpp

---
+++ DSA/Arrays/2SumVariety1Optimal.cpp
@@ -0,0 +1,22 @@
+//T.C - O(n) + O(n logn) for sorting
+//S.C - O(1) or O(n) for considering the sorting
+
+string read(int n, vector<int> book, int target)
+{
+    sort(book.begin(), book.end());
+    int i = 0;
+    int j = n-1;
+    
+    while(i < j){
+        if((book[i]+book[j]) == target) {
+            return "YES";
+        }
+
+        if((book[i]+book[j]) < target){
+            i++;
+        }else{
+            j--;
+        }
+    }
+    return "NO";
+}
