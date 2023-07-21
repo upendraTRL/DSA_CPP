0dafcbb209b922661eaafd88c208b061369d8f1b
upendraTRL <96102665+upendraTRL@users.noreply.github.com>
2023-07-21 16:56:39 +0530

    Create 2SumVariety2Optimal.cpp

---
+++ DSA/Arrays/2SumVariety2Optimal.cpp
@@ -0,0 +1,22 @@
+//TC - O(n logn) for ordered map
+//     O(n^2) - W.C, O(n) - B.C/A.C
+//SC - O(n) for map
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mpp;
+        int n = nums.size();
+        
+        for(int i=0; i<n; i++) {
+            int num = nums[i];
+            int moreN = target - num;
+
+            if(mpp.find(moreN) != mpp.end()) {
+                return {mpp[moreN], i};
+            }
+            mpp[num] = i;
+        }
+        return {-1, -1};
+    }
+};
