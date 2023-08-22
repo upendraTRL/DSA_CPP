18b21b160a10c1a88abb53ee981ceec74cfc5ff1
Upendra Taral <96102665+upendraTRL@users.noreply.github.com>
2023-08-22 11:49:03 +0530

    Create searchBST.cpp

---
+++ DSA/Trees/searchBST.cpp
@@ -0,0 +1,19 @@
+//TC - O(logn)
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    BinaryTreeNode<int> *temp = root;
+    
+    while(temp != NULL) {
+        if(temp->data == x) {
+            return true;
+        }
+
+        if(temp->data > x){
+            temp = temp->left;
+        }else {
+            temp = temp->right;
+        }
+    }
+
+    return false;
+}
